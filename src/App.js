155d6c8ccb13b57e62d62978c3cc83403bb87abe
StephanBegins <stephan1217at@gmail.com>
2025-01-24 14:54:11 +0530

    first update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Concrete Calculator Form</h1>
+        <table className="calculator-table">
+          <thead>
+            <tr>
+              <th>+</th>
+              <th>S.no</th>
+              <th>Type of Calculations</th>
+              <th>Measurements</th>
+              <th>Units</th>
+              <th>Remarks</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>+</td>
+              <td>1</td>
+              <td>Concrete Volume</td>
+              <td>Length x Width x Height</td>
+              <td>Cubic Yards</td>
+              <td>Enter dimensions to calculate volume</td>
+            </tr>
+          </tbody>
+        </table>
       </header>
     </div>
   );
